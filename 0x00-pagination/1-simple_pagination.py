4e47b116ef113a2175fcaa163aa10964b257bbf8
lonebhen <ben.angmortey@gmail.com>
2023-02-21 05:39:15 +0000

    csv

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,55 @@
+#!usr/bin/env python3
+"""
+
+"""
+
+import csv
+import math
+from typing import List, Tuple
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+            find the correct indexes to paginate the dataset correctly
+            and return the appropriate page of the dataset
+        """
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
+
+        self.dataset()
+
+        i = index_range(page, page_size)
+        if i[0] >= len(self.__dataset):
+            return []
+        return self.__dataset[i[0]:i[1]]
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    return a tuple of size two containing a start index and an end index
+    corresponding to the range of indexes to return in a list for those
+    pagination parameters
+    """
+
+    index = (page*page_size) - page_size
+    index_1 = index + page_size
+    return (index, index_1)
