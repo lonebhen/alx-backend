f0434a71d131216db922f7522e09113b6f669b97
lonebhen <ben.angmortey@gmail.com>
2023-02-21 05:26:33 +0000

    page and page size

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -11,6 +11,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     return a tuple of size two containing a start index and an end index
     corresponding to the range of indexes to return in a list for those
+    pagination parameters
     """
 
     index = (page*page_size) - page_size
