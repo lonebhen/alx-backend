584452c92e31c758b6817235ada09283e80dbf8b
lonebhen <ben.angmortey@gmail.com>
2023-03-03 23:25:21 +0000

    locale

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+
+from flask import Flask, request, render_template
+from babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCAL = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route("/", methods=['GET'], strict_slashes=False)
+def hello_world():
+    return render_template('3')
+
+
+if __name__ == '__main__':
+    app.run()
