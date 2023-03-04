b7bca1808aca2c071d4ab9219211032db289e975
lonebhen <ben.angmortey@gmail.com>
2023-03-04 00:02:02 +0000

    lpcale fr

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -17,12 +17,14 @@ class Config:
 app.config.from_object(Config)
 
 
-@babel.localeselector
 def get_locale():
     loc = request.args.get('locale')
     if loc in app.config['LANGUAGES']:
         return loc
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route("/", methods=['GET'], strict_slashes=False)
