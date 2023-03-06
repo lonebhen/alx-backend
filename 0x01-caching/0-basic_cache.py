78bec59c55a7a8de78f56562a2c15a9349667459
lonebhen <ben.angmortey@gmail.com>
2023-03-06 16:41:59 +0000

    basic caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+
+"""Caching"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+
+    def put(self, key, item):
+        """Put method"""
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Get method"""
+        if key in self.cache_data:
+            return self.cache_data[key]
