cadb5cf551d155ac17148099da894c10226e4ae0
lonebhen <ben.angmortey@gmail.com>
2023-03-15 23:26:34 +0000

    babel

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -16,7 +16,7 @@ babel = Babel(app)
 class Config(object):
     """Config Languages and timezones"""
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCAL = "en"
+    BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
