365b4c4f2f2916cc03ac6d1a3b98d89c060b7d35
lonebhen <ben.angmortey@gmail.com>
2023-03-15 23:39:03 +0000

    3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,32 +1,36 @@
 #!/usr/bin/env python3
 
+"""
+    Languages
+"""
 
 from flask import Flask, request, render_template
-from flask_babel import Babel, gettext
+from flask_babel import Babel
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config:
+class Config(object):
+    """lang config"""
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCAL = "en"
+    BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app.config.from_object(Config)
 
 
+@babel.localeselector
 def get_locale():
+    """best language"""
     return request.accept_languages.best_match(Config.LANGUAGES)
-
-
-babel.init_app(app, locale_selector=get_locale)
 
 
 @app.route("/", methods=['GET'], strict_slashes=False)
 def hello_world():
-    return render_template('3-index.html')
+    """Hello"""
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
